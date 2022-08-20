4806350e55e77169da42ca77ec97507d636c777b
kainaat0110 <110599444+kainaat0110@users.noreply.github.com>
2022-08-20 20:34:49 +0530

    2nd program

---
+++ main 2.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x , y , sum;
+    x = 5;
+    y = 12;
+    sum = x + y;
+    printf("The Sum of x and y is %d" , sum);
+
+    return 0;
+}
