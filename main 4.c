1ceed53b73b007a2569104b8b44f33927a47a4ca
kainaat0110 <110599444+kainaat0110@users.noreply.github.com>
2022-08-21 21:37:11 +0530

    3rd program

---
+++ main 4.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x , table;
+    x = 5;
+    for(int i = 1; i <= 10; i++);
+    {
+        table = x * i;
+        printf("%d\n" , table);
+    }
+
+    return 0;
+}
