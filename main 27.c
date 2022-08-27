255e813a0b4e6c16db023c394a0f09eaf3e2129e
kainaat0110 <110599444+kainaat0110@users.noreply.github.com>
2022-08-28 00:23:07 +0530

    Day 10

---
+++ main 27.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=0; i<=10;i++)
+    {
+        printf("%d\n" , i);
+    }
+    
+
+    return 0;
+}
