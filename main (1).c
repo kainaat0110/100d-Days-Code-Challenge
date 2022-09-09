c18c36fcedc0d07c4af4fb77ca64bd04749e373a
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-09 23:26:57 +0530

    Day 22

--- main (1).c
+++ main (1).c
@@ -7,23 +7,22 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
+#include<string.h>
+struct student {
+        int roll_no;
+        char name[50];
+        float cgpa;
+    };
 
 int main()
 {
-    int x , y , max;
-    printf("Enter any two numbers:\n");
-    scanf("%d %d" , &x , &y);
-    max = (x > y ? x : y);
-    while(1)
-    {
-        if(max % x == 0 && max % y == 0)
-        {
-            printf("%d" , max);
-            break;
-        }
-        ++max;
-    }
+    struct student s;
+    s.roll_no = 1978;
+    strcpy(s.name , "Kainaat");
+    s.cgpa = 8.3;
+    printf("Name is %s\n" , s.name);
+    printf("Roll no is %d\n" , s.roll_no);
+    printf("CGPA is %f\n" , s.cgpa);
     
-   
     return 0;
 }
