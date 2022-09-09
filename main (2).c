65737b79411084c31b27513f3480b26c47b30765
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-10 00:15:15 +0530

    Day 23

--- main (2).c
+++ main (2).c
@@ -7,28 +7,23 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-int reverse(int n);
+#include<string.h>
+struct student {
+        int roll_no;
+        char name[60];
+        float cgpa;
+    };
 
 int main()
 {
-    int n;
-    printf("Enter any number:");
-    scanf("%d" , &n);
-    printf("The reverse of the number is : %d" , reverse(n));
-
-    return 0;
-}
-
-int reverse(int n)
-{
-    int r , sum ;
-    sum = 0;
-    while(n != 0)
-    {
-        r = n % 10;
-        sum = sum * 10 + r;
-        n = n /10;
-    }
-    return sum;
+    struct student s;
+    printf("Enter your name, roll_no , cgpa : \n");
+    scanf("%s" , s.name);
+    scanf("%d" , &s.roll_no);
+    scanf("%f" , &s.cgpa);
+    printf("Name is %s\n" , s.name);
+    printf("Roll no is %d\n" , s.roll_no);
+    printf("CGPA is %f\n" , s.cgpa);
     
+    return 0;
 }
