a6b85cccb6e9c1175da6e5be990268f767d6a878
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-11 21:54:13 +0530

    Day 24

--- main (4).c
+++ main (4).c
@@ -10,12 +10,29 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    float price[3];
-    printf("Enter 3 prices:");
-    scanf(" %f %f %f" , &price[0], &price[1] , &price[2]);
-    printf("The final price of first product is (including gst) : %f\n" , price[0] + (0.18*price[0]));
-    printf("The final price of the second product is (including gst) : %f\n" , price[1] + (0.18*price[1]));
-    printf("The final price of third price is(including gst) :%f\n" , price[2] + (0.18*price[2]));
-
+    int a[5];
+    printf("Enter any 5 elements of an array: ");
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d" , &a[i]);
+    }
+    
+    for(int i = 0; i<5 ; i++)
+    {
+        for(int j = i+1 ; j < 5; j++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp;
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d\t" , a[i]);
+    }
     return 0;
 }
