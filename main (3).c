835280e7460e30b8db80f2a6f8c7c89fcf297702
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-11 21:52:52 +0530

    Day 24

--- main (3).c
+++ main (3).c
@@ -7,24 +7,36 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
+#include<string.h>
+struct student {
+        int roll_no;
+        char name[60];
+        float cgpa;
+    };
 
 int main()
 {
-    int x , fibx , fiby , fibz;
-    printf("Enter a number:");
-    scanf("%d\t" , &x);
-    fibz =  0;
-    printf("%d" , fibz);
-    fibx = 1;
-    printf("%d\t" , fibx);
-    for(int i =0; i <= x; i++)
-    {
-        fiby = fibx + fibz;
-        fibz = fibx;
-        fibx = fiby;
-        printf("%d\t" , fiby);
-    }
+    struct student s1 , s2 , s3;
     
+   
+    printf("Enter first student name, roll_no , cgpa : \n");
+    scanf("%s" , s1.name);
+    scanf("%d" , &s1.roll_no);
+    scanf("%f" , &s1.cgpa);
+    printf("Enter second student name, roll_no , cgpa : \n");
+    scanf("%s" , s2.name);
+    scanf("%d" , &s2.roll_no);
+    scanf("%f" , &s2.cgpa);
+    printf("Enter third student name, roll_no , cgpa : \n");
+    scanf("%s" , s3.name);
+    scanf("%d" , &s3.roll_no);
+    scanf("%f" , &s3.cgpa);
 
+
+    printf("Details of First student is\n Name : %s\n Roll no : %d\n CGPA : %f\n" , s1.name , s1.roll_no , s1.cgpa);
+    printf("Details of Second student is\n Name : %s\n Roll no : %d\n CGPA : %f\n" , s2.name , s2.roll_no , s2.cgpa);
+    printf("Details of Third student is\n Name : %s\n Roll no : %d\n CGPA : %f\n" , s3.name , s3.roll_no , s3.cgpa);
+    
+    
     return 0;
 }
