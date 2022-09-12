848b43fcad8ec50302425a62730377aaf8e55bce
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-12 21:57:05 +0530

    Day 25

--- main (5).c
+++ main (5).c
@@ -7,54 +7,32 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-int sum(int x , int y);
-int diff(int x , int y);
-int mul(int x , int y);
-int divi(int x , int y);
 
 int main()
 {
-    int x , y;
-    char op;
-    printf("Select an operator :\n");
-    scanf("%s" , &op);
-    printf("Enter two positive numbers:\n");
-    scanf("%d %d" , &x , &y);
-    switch(op)
+    int a[5];
+    printf("Enter any 5 elements of an array: ");
+    for(int i = 0; i < 5; i++)
     {
-        case '+' : printf("Sum is equal to %d" , sum(x,y));
-                  break;
-        case '-' : printf("Difference is equal to %d" ,diff(x,y));
-                  break;
-        case '*' : printf("Multiplication is equal to %d" , mul(x,y));
-                  break;
-        case '/' :printf("Divison is equal to %d" , divi(x,y));
-                  break;
-        default : printf("Invalid operator.\n");
+        scanf("%d" , &a[i]);
     }
-
+    
+    for(int i = 0; i<5 ; i++)
+    {
+        for(int j = i + 1 ; j < 5; j++)
+        {
+            if(a[j]>a[i])
+            {
+                int temp;
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d\t" , a[i]);
+    }
     return 0;
-}
-int sum(int x , int y)
-{
-    int sum = x + y;
-    return sum;
-}
-
-int diff(int x , int y)
-{
-    int diff = x - y;
-    return diff;
-}
-
-int mul(int x , int y)
-{
-    int mul = x * y;
-    return mul;
-}
-
-int divi(int x , int y)
-{
-    int division = x/y;
-    return division;
 }
