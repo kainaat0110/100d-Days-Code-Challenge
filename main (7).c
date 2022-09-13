4507c486c13214b4c31f2b0290e087d8c8da009b
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-09-13 22:11:31 +0530

    Day 26

--- main (7).c
+++ main (7).c
@@ -8,29 +8,28 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
-int table(int arr[][10] , int m , int n , int num);
-
 int main()
 {
-    int tables[2][10];
-    table(tables , 0 , 10 , 2);
-    table(tables , 1 , 10 , 3);
-    for(int i = 1; i <= 10; i++)
+    int array[3]={5,7,9};
+    for(int i =0; i< 3;i++)
     {
-        printf("%d\t" , tables[0][i]);
+        for(int j = i+1 ; j < 3; j++)
+        {
+            if(array[j] < array[i])
+            {
+                int temp;
+                temp = array[i];
+                array[i] = array [j];
+                array[j] = temp;
+            }
+        }
     }
-    for(int i = 1; i <= 10; i++)
+    printf("Array i Ascending order is:\n");
+    for(int i = 0; i < 3; i++)
     {
-        printf("%d\t" , tables[1][i]);
+        printf("%d\t" , array[i]);
+
     }
     
-
     return 0;
-}
-int table(int arr[][10] , int m , int n , int num)
-{
-    for(int i = 1; i <= 10; i++)
-    {
-        arr[n][i] = num * i;
-    }
 }
