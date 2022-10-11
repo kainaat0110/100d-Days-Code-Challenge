25bddf819904f6d9d5165831513a1043be000986
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-10-11 21:55:44 +0530

    Day 49

---
+++ javaprog.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int num;
+        num = 10;
+        System.out.println("Num is\n" +num);
+        num = num*20;
+        String newline = System.lineSeparator();
+        System.out.println("Now new num is\n" +num);
+    }
+}
