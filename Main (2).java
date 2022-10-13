ca7dd3d83f692913fbd3dfc29c465f401b1cc47f
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-10-13 21:41:55 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int y = 20;
+	    
+	    for(int x = 0; x <= 10; x++){
+	        
+	        System.out.println("X is " +x);
+	        System.out.println("Y is " +y);
+	        
+	        y = y - 2;
+	    }
+		
+	}
+}
