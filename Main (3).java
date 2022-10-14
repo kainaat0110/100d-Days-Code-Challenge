f456950b6b35061bb3d9ad7c639799276c84576c
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-10-14 22:10:52 +0530

    Day 52

---
+++ Main (3).java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int lightspeed;
+	    long days, seconds, distance;
+	    lightspeed = 186000;
+	    days = 1000;
+	    seconds = days * 24 * 60 * 60;
+	    distance = seconds * lightspeed;
+		System.out.println("Distance = " +distance);
+	}
+}
