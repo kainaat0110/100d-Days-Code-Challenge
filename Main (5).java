82255c739035549ebed62e1a06b9cdeedddc2aa7
Kainaat Zaidi <110599444+kainaat0110@users.noreply.github.com>
2022-10-15 22:07:10 +0530

    Day 53

---
+++ Main (5).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    char ch1 , ch2;
+	    ch1 = 88;
+	    ch2 = 'Y';
+		System.out.println("ch1 and ch2");
+		System.out.println(ch1+ " " +ch2);
+	}
+}
